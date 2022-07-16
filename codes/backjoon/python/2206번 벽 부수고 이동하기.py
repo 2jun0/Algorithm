e2615c6ddc6798d0f42d4f5d912cf51ee8f26b86
2jun0 <soo28819@naver.com>
2022-07-16 23:27:00 +0900

    Update: 2206 Problem Code

--- codes/backjoon/python/2206번 벽 부수고 이동하기.py
+++ codes/backjoon/python/2206번 벽 부수고 이동하기.py
@@ -1,46 +1,42 @@
 import sys
-sys.setrecursionlimit(10000000) # 재귀 제한 풀기
+from collections import deque
+
 INF = 10**8
 def input(_type=str):
 	return _type(sys.stdin.readline().strip())
 def input_n(_type=str):
 	return list(map(_type, input().split()))
-def print_n(L, join_str=' '):
-  s = ''
-  for l in L[:-1]: s+='{}{}'.format(l, join_str)
-  s+='{}'.format(L[-1])
-  print(s)
 
 N, M = input_n(int)
 table = []
 for _ in range(N):
-  L = []
-  for c in input(): L.append(int(c))
-  table.append(L)
+  table.append(list(map(int, input())))
 
-visited = [[[False, False] for _ in range(M)] for _ in range(N)]
-dist = [[[INF, INF] for _ in range(M)] for _ in range(N)]
+def bfs():
+  dist = [[[INF] * 2 for _ in range(M)] for _ in range(N)]
+  dist[0][0][0] = 1
 
-def conflict(i,j,d,b):
-  if not (0<=i<N and 0<=j<M): return True
-  if dist[i][j][b] <= d or visited[i][j][b]: return True
-  if b==1 and table[i][j]==1: return True
-  return False
+  # i, j, break?
+  q = deque()
+  q.append((0,0,0))
+  while q:
+    i, j, b = q.popleft()
 
-# i, j, d, break?
-s1 = []
-s2 = [(0,0,0)]
-d = 1
-while len(s2) > 0:
-  s2, s1 = s1, s2
-  while len(s1) > 0:
-    i, j, b = s1.pop()
-    if conflict(i,j,d,b): continue
-    if b == 0 and table[i][j] == 1: b = 1
-    visited[i][j][b] = True
-    dist[i][j][b] = d
-    T = [(i-1,j,b),(i+1,j,b),(i,j-1,b),(i,j+1,b)]
-    s2.extend(T)
-  d+=1
-result = min(dist[-1][-1][0], dist[-1][-1][1])
-print(result if result < INF else -1)
+    # 도착지에 도달했으면 끝
+    if i == N-1 and j == M-1:
+      return dist[i][j][b]
+
+    for t_i, t_j in [(i-1,j),(i+1,j),(i,j-1),(i,j+1)]:
+      if 0<=t_i<N and 0<=t_j<M:
+        # 갈 수 있고, 방문하지 않음
+        if table[t_i][t_j] == 0 and dist[t_i][t_j][b] == INF:
+          dist[t_i][t_j][b] = dist[i][j][b]+1
+          q.append((t_i, t_j, b))
+        # 벽이 있고 부술 수 있음
+        if table[t_i][t_j] == 1 and b == 0:
+          dist[t_i][t_j][1] = dist[i][j][b]+1
+          q.append((t_i, t_j, 1))
+      
+  return -1
+
+print(bfs())
