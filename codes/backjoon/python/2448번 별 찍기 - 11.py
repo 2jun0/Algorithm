9215adadd6b1d5f164efd33321cfad761a90d551
2jun0 <soo28819@naver.com>
2022-07-26 01:59:07 +0900

    Modify: turn codes to easy

--- codes/backjoon/python/2448번 별 찍기 - 11.py
+++ codes/backjoon/python/2448번 별 찍기 - 11.py
@@ -6,24 +6,27 @@ def input(_type=str):
 triangle=[
                 (-2,2),
          (-1,1),       (-1,3),
-  (0,0), (0,1), (0,2), (0,3), (0,4)]
+  (0,0), (0,1), (0,2), (0,3), (0,4)
+]
+
+big_triangle=[
+       (-.5,.5),
+  (0,0),       (0,1),
+]
+
 N = input(int)
-result = [[" "]*(2*N) for _ in range(N)]
+results = [[" "]*(2*N) for _ in range(N)]
 
 def func(y, x, width):
-  # 밑면은 특이하게 2배가 큼.
   # 가장 작은 삼각형의 밑면이 6개임
   if width==6:
-    for ty, tx in triangle: result[ty+y][tx+x] = '*'
-    return
-
-  width_nxt = width//2
-  height_nxt = width_nxt//2
-
-  func(y, x, width_nxt) # 아래 왼쪽
-  func(y, x+width_nxt, width_nxt) # 아래 오른쪽
-  func(y-height_nxt, x+width_nxt//2, width_nxt) # 위
+    for dy, dx in triangle: 
+      results[y+dy][x+dx] = '*'
+  else:
+    width_nxt = width//2
+    for dy, dx in big_triangle:
+      func(int(y+dy*width_nxt), int(x+dx*width_nxt), width_nxt)
 
 # 아래 왼쪽부터 시작.
-func(N-1, 0, 2*N-1)
-print('\n'.join([''.join(L) for L in result]))
+func(N-1, 0, 2*N)
+print('\n'.join([''.join(L) for L in results]))
