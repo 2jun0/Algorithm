357c7c597ee4792abedce72d8c148db5eadcecc1
2jun0 <soo28819@naver.com>
2022-08-20 23:35:28 +0900

    modify: remove non-used function in problem 14890

--- codes/backjoon/python/14890번 경사로.py
+++ codes/backjoon/python/14890번 경사로.py
@@ -5,11 +5,6 @@ def input(_type=str):
 	return _type(sys.stdin.readline().strip())
 def input_n(_type=str):
 	return list(map(_type, input().split()))
-def print_n(L, join_str=' '):
-  for i,l in enumerate(L): print(l, end=join_str if i<len(L)-1 else '\n')
-def LL(n,m,d=0): return [[d]*n for _ in range(m)]
-def avg(l): return sum(l)/len(l)
-def getLRUD(i,j): return [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]
 
 N, L = input_n(int)
 
