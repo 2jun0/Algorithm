0f6819b68727438ea74a96329d4060b928539ef7
2jun0 <soo28819@naver.com>
2022-09-01 19:23:10 +0900

    modify: renew a problem 17143

--- codes/backjoon/python/17143번 낚시왕.py
+++ codes/backjoon/python/17143번 낚시왕.py
@@ -1,91 +1,95 @@
 import sys
-INF = 10**10
 def input(_type=str):
 	return _type(sys.stdin.readline().strip())
 def input_n(_type=str):
 	return list(map(_type, input().split()))
-def print_n(L, join_str=' '):
-  for i,l in enumerate(L): print(l, end=join_str if i<len(L)-1 else '\n')
-def LL(n,m,d=0): return [[d]*n for _ in range(m)]
-def avg(l): return sum(l)/len(l)
 
 UP, DOWN, RIGHT, LEFT = 1, 2, 3, 4
 
-R, C, M = input_n(int)
-
 class Shark:
-  def __init__(self, v):
-    self.idx = 0
+  def __init__(self, y, x, speed, d, size):
     self.is_live = True
-    self.r = v[0]-1
-    self.c = v[1]-1
-    self.s = v[2]
-    self.d = v[3]
-    self.z = v[4]
 
-    if self.d == LEFT and self.c == 0: self.d = RIGHT
-    if self.d == RIGHT and self.c == C-1: self.d = LEFT
-    if self.d == UP and self.r == 0: self.d = DOWN
-    if self.d == DOWN and self.r == R-1: self.d = UP
+    self.y = y
+    self.x = x
+    self.speed = speed
+    self.d = d
+    self.size = size
 
+  def move(self):
+    if self.d == LEFT:
+      self.x -= self.speed
+    elif self.d == RIGHT:
+      self.x += self.speed
+    elif self.d == UP:
+      self.y -= self.speed
+    elif self.d == DOWN:
+      self.y += self.speed
 
+    while True:
+      if self.x < 0:
+        self.x = 0 + (0 - self.x)
+        self.d = RIGHT  
+      elif self.x >= C:
+        self.x = (C-1) - (self.x - (C-1))
+        self.d = LEFT
+      elif self.y < 0:
+        self.y = 0 + (0 - self.y)
+        self.d = DOWN
+      elif self.y >= R:
+        self.y = (R-1) - (self.y - (R-1))
+        self.d = UP
+      else:
+        break
+      
 
-S = [Shark(input_n(int)) for _ in range(M)]
-
+R, C, M = input_n(int)
 table = [[None]*C for _ in range(R)]
-for s in S: table[s.r][s.c] = s
+nxt_table = [[None]*C for _ in range(R)]
+S = []
+
+for _ in range(M):
+  y, x, s, d, z = input_n(int)
+  y, x = y-1, x-1
+
+  s = Shark(y,x,s,d,z)
+  S.append(s)
+  table[y][x] = s
 
 sum_z = 0
-for pc in range(C):
+for px in range(C):
   # 낚았다!
-  for pr in range(R):
-    if table[pr][pc]:
-      sum_z += table[pr][pc].z
-      table[pr][pc].is_live = False
-      table[pr][pc] = None
-
+  for py in range(R):
+    if table[py][px]:
+      sum_z += table[py][px].size
+      table[py][px].is_live = False
+      table[py][px] = None
       break
+
   # 움직였다!
   for s in S:
-    if not s.is_live: continue
+    # 죽은 경우 제외
+    if not s.is_live: continue 
 
-    if table[s.r][s.c] == s: 
-      table[s.r][s.c] = None
-    s.idx += 1
-    if s.d in [LEFT, RIGHT]:
-      if s.d == RIGHT: c = s.c
-      else: c = 2*(C-1)-s.c
-      c=(c+s.s)%(2*(C-1))
+    table[s.y][s.x] = None
+    
+    s.move()
 
-      if c < C-1:
-        s.d = RIGHT
-        s.c = c
+    # 상어 인카운트 시
+    if nxt_table[s.y][s.x]:
+      other = nxt_table[s.y][s.x]
+      
+      if s.size > other.size:
+        # 내가 먹음
+        other.is_live = False
+        nxt_table[s.y][s.x] = s
       else:
-        s.d = LEFT
-        s.c = 2*(C-1)-c
-    if s.d in [UP, DOWN]:
-      if s.d == DOWN: r = s.r
-      else: r = 2*(R-1)-s.r
-      r=(r+s.s)%(2*(R-1))
-
-      if r < R-1:
-        s.d = DOWN
-        s.r = r
-      else:
-        s.d = UP
-        s.r = 2*(R-1)-r
-
-    # 상어 인카운트
-    if table[s.r][s.c] and table[s.r][s.c].idx == s.idx:
-      # 내가 먹음
-      if table[s.r][s.c].z < s.z:
-        table[s.r][s.c].is_live = False
-        table[s.r][s.c] = s
-      # 내가 먹힘
-      else:
+        # 내가 먹힘
         s.is_live = False
     else:
-      table[s.r][s.c] = s
+      nxt_table[s.y][s.x] = s
+  
+  table, nxt_table = nxt_table, table
     
 print(sum_z)
     
