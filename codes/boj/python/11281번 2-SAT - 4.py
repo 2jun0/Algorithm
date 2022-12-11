5f6dcbef94be2cff5ebd6ba2e3bbc24141a2b47a
2jun0 <soo28819@gmail.com>
2022-12-11 23:39:19 +0900

    add: two boj problems

---
+++ codes/boj/python/11281번 2-SAT - 4.py
@@ -0,0 +1,111 @@
+import sys
+from collections import defaultdict
+sys.setrecursionlimit(100000)
+
+def input(_type=str):
+	return _type(sys.stdin.readline().strip())
+def input_n(_type=str):
+	return list(map(_type, input().split()))
+
+def find_sccs(graph: dict, N):
+  labels = defaultdict(lambda: None)
+  label_counter = 1
+  finished = defaultdict(lambda: False)
+  stack = []
+  sccs = []
+  
+  def get_scc(end):
+    scc = []
+    while True:
+      x = stack.pop()
+      finished[x] = True
+      scc.append(x)
+      
+      if x == end:
+        break
+      
+    return scc
+  
+  def dfs(x):
+    nonlocal label_counter
+    
+    labels[x] = label_counter
+    label_counter += 1
+    parent = labels[x]
+    stack.append(x)
+    
+    for nxt in graph[x]:
+      if not labels[nxt]: # 방문하지 않은 경우 -> 다음 노드로
+        parent = min(parent, dfs(nxt))
+      elif not finished[nxt]: # 끝나지 않은 경우 -> cycle에 편입
+        parent = min(parent, labels[nxt])
+    
+    # 탐색이 끝난경우, 자신이 지금 트리의 부모라면
+    if parent == labels[x]:
+      sccs.append(get_scc(x))
+    
+    return parent
+  
+  for x in range(1,N+1):
+    for rx in [x, -x]:
+      if not labels[rx]:
+        dfs(rx)
+  
+  return sccs
+
+def check(sccs):
+  '''-x, x가 같은 사이클에 있는 지 확인'''
+  for scc in sccs:
+    for x in scc:
+      if -x in scc:
+        return False
+      
+  return True
+
+def get_result(sccs: list, graph, N):
+  class SCC:
+    def __init__(self, scc):
+      self.scc = scc
+    def __lt__(self, other):
+      for x in self.scc:
+        for y in other.scc:
+          if y in graph[x]: # x -> y
+            return True
+          if x in graph[y]: # y -> x
+            return False
+      
+      return True
+    def __repr__(self):
+      return str(self.scc)
+  
+  sccs = [SCC(scc) for scc in sccs]
+  sccs.sort()
+  
+  result = [None]*(N+1)
+  for scc in sccs:
+    for x in scc.scc:
+      if result[abs(x)] != None:
+        continue
+      
+      if x > 0:
+        result[x] = 0
+      else:
+        result[-x] = 1
+  return result
+
+N, M = input_n(int)
+graph = defaultdict(list)
+
+for _ in range(M):
+  a, b = input_n(int)
+  graph[-a].append(b)
+  graph[-b].append(a)
+
+sccs = find_sccs(graph, N)
+
+if check(sccs):
+  print(1)
+  result = get_result(sccs, graph, N)
+  print(*result[1:])
+else:
+  print(0)
