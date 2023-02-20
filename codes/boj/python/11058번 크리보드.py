85eecdbd3c19ec7a27eef5bafca7011fe6c604bb
2jun0 <soo28819@gmail.com>
2023-02-20 20:21:23 +0900

    add: a boj problem 11058:크리보드

--- codes/boj/python/11058번 크리보드.py
+++ codes/boj/python/11058번 크리보드.py
@@ -1,5 +1,5 @@
 import sys
-from collections import defaultdict
+from collections import deque
 
 def input(_type=str):
 	return _type(sys.stdin.readline().rstrip())
@@ -8,26 +8,36 @@ def input_n(_type):
 
 # cmd type
 # A
-# Ctrl-A, Ctrl-C
+# Ctrl-A, Ctrl-C, Ctrl-V
 # Ctrl-V
+
+def bfs():
+  dp = [[-1]*(N+1) for _ in range(N+4)]
+  qs = [deque() for _ in range(N+4)]
+  dp[0][0] = 0
+  qs[0].append(0)
+  
+  for i in range(N):
+    while qs[i]:
+      ci = qs[i].popleft()
+      
+      if dp[i+1][ci] == -1:
+        qs[i+1].append(ci)
+      
+      # A
+      dp[i+1][ci] = max(dp[i+1][ci], dp[i][ci]+1)
+      # Ctrl-V
+      dp[i+1][ci] = max(dp[i+1][ci], dp[i][ci] + max(dp[ci]))
+      
+      if dp[i+3][i] == -1:
+        qs[i+3].append(i)
+      
+      # Ctrl-a Ctrl-c Ctrl-v
+      dp[i+3][i] = max(dp[i+3][i], 2*max(dp[i]))
+      
+  return max(dp[N])
+  
 
 N = input(int)
 
-# (turn, 복사 위치 기본이 0 없음)
-dp = [[-1]*(N+1) for _ in range(N+4)]
-dp[0][0] = 0
-for i in range(0, N):
-  # A
-  for ci in range(N):
-    if dp[i][ci] == -1: continue
-    
-    dp[i+1][ci] = max(dp[i+1][ci], dp[i][ci]+1)
-  
-  for ci in range(N):
-    if dp[i][ci] == -1: continue
-    # Ctrl-V
-    dp[i+1][ci] = max(dp[i+1][ci], dp[i][ci] + max(dp[ci]))
-  # Ctrl-a Ctrl-c Ctrl-vf
-  dp[i+3][i] = max(dp[i+3][i], 2*max(dp[i]))
-
-print(max(dp[N]))
+print(bfs())
