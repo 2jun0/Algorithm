273a0adf1e8ad8ef5c21a508b24f780a29f4f745
2jun0 <soo28819@gmail.com>
2023-02-27 22:55:07 +0900

    remove: wrong files

--- codes/boj/python/13597번 Tri－du.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-def input(type=str):
-  return type(sys.stdin.readline().strip())
-def input_n(type=str):
-  return list(map(type, input().split()))
-
-A, B = input_n(int)
-print(max(A,B))
