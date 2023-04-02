cc74d7225c3097721244f4f7f6495b39c459d612
2jun0 <soo28819@gmail.com>
2023-04-02 23:28:45 +0900

    add: unfinished problem 1014

--- codes/boj/python/1014번 컨닝.py
+++ codes/boj/python/1014번 컨닝.py
@@ -1,53 +1,30 @@
 import sys
 
-def input(type=str):
-  return type(sys.stdin.readline().strip())
-def input_n(type=str):
-  return list(map(type, input().split()))
+def input(_type=str):
+	return _type(sys.stdin.readline().strip())
+def input_n(_type=str):
+	return list(map(_type, input().split()))  
 
 dy = [-1, -1, 0, 0]
 dx = [-1, 1, -1, 1]
 
-def dfs(A, idx):
-  y, x = A[idx]
-  
-  if occupied[y][x] or table[y][x] == 'x':
-    return 0
-  
-  # place 
-  logs = [(y,x)]
-  occupied[y][x] = True
-  for i in range(4):
-    ny = y + dy[i]
-    nx = x + dx[i]
-    
-    if 0<=ny<N and 0<=nx<M and not occupied[ny][nx]:
-      occupied[ny][nx] = True
-      logs.append((ny,nx))
-      
-  # next
-  max_nxt_cnt = 0
-  for nidx in range(idx+1, len(A)):
-    max_nxt_cnt = max(max_nxt_cnt, dfs(A, nidx))
-      
-  # leave
-  for ly, lx in logs:
-    occupied[ly][lx] = False
-  
-  return max_nxt_cnt + 1
-
 C = input(int)
 for _ in range(C):
   N, M = input_n(int)
-  table = [input() for _ in range(N)]
-  occupied = [[False]*M for _ in range(N)]
+  table = [''] + [input() for _ in range(N)]
+  dp = [[-1]*(1<<M) for _ in range(N+1)]
   
-  A = []
-  for y in range(N):
+  '''모두 없는 경우도 존재할 수 있음.'''
+  for y in range(1, N+1):
+    dp[y][0] = 0
+  
+  for y in range(1, N+1):
     for x in range(M):
-      A.append((y,x))
+      if table[y][x] != '.':
+        continue
+      
+      # left up
+      if dp[y-1][x-1] >= 0:
+        dp[y][x] = dp[y-1][x-1]
+        
   
-  rs = 0
-  for idx in range(len(A)):
-    rs = max(rs, dfs(A,idx))
-  print(rs)
