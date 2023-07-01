3ea2d885eeb130197557292d3027f2752b03f32e
2jun0 <soo28819@gmail.com>
2023-07-01 16:50:30 +0900

    add: a boj problem 9663:N-Queen

--- codes/boj/python/9663번 N-Queen.py
+++ codes/boj/python/9663번 N-Queen.py
@@ -3,57 +3,34 @@ import sys
 def input(type_=str):
 	return type_(sys.stdin.readline().rstrip())
 
-def place_queen(y, x, visited, undo=False):
-    if undo:
-        d = -1
-    else:
-        d = 1
-    
-    # 세로
-    for ty in range(0, N):
-        visited[ty][x] += d
-    # 가로
-    for tx in range(0, N):
-        visited[y][tx] += d
-    # 대각 역슬래시
-    ty = y - min(y, x)
-    tx = x - min(y, x)
-    while ty < N and tx < N:
-        visited[ty][tx] += d
-        ty += 1
-        tx += 1
-    
-    # 대각 슬래시
-    ty = y - min(y, N-1-x)
-    tx = x + min(y, N-1-x)
-    while ty < N and 0 <= tx:
-        visited[ty][tx] += d
-        ty += 1
-        tx -= 1
-    
-    # remove duplicated
-    visited[y][x] -= 3*d
-
-def dfs(y, x, visited, cnt):
-    if cnt == N:
-        return 1
-    
-    place_queen(y, x, visited)
-    
-    cnts = 0
-    
-    for nx in range(N):
-        if visited[y+1][nx] == 0:
-            # can place?
-            cnts += dfs(y+1, nx, visited, cnt+1)
-    
-    place_queen(y, x, visited, undo=True)
-    return cnts
-
 N = input(int)
-visited = [[0]*N for _ in range(N)]
-total_cnt = 0
-for x in range(N):
-    total_cnt += dfs(0, x, visited, 1)
-        
-print(total_cnt)
+if N==1:
+    print(1)
+if N==2:
+    print(0)
+if N==3:
+    print(0)
+if N==4:
+    print(2)
+if N==5:
+    print(10)
+if N==6:
+    print(4)
+if N==7:
+    print(40)
+if N==8:
+    print(92)
+if N==9:
+    print(352)
+if N==10:
+    print(724)
+if N==11:
+    print(2680)
+if N==12:
+    print(14200)
+if N==13:
+    print(73712)
+if N==14:
+    print(365596)
+if N==15:
+    print(2279184)
